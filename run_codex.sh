580872955d5f9df428e6a42be5123cf08ccbcf72
FeiSong1234 <xsong2008@126.com>
2026-03-10 19:32:22 +0800

    add LucidNFT

--- run_codex.sh
+++ run_codex.sh
@@ -1,5 +1,5 @@
 # Set environment variable
-export CODEX_API_KEY='sk-8dKTLmuNYRbi4EuyibEpE6EURmYv037fZ1IggTrRU4lpE1MM'
+export CODEX_API_KEY='sk-ant-oat01-d6f1024d2ea67a4b6b9ba44e93462e3c630dbc040db01b83d6e4954c37b07e55'
 
 # Run Codex
 codex
