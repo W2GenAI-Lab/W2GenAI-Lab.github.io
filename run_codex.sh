7989d3560b9fc9de44ab2ed2f5caff1ce6e5d634
FeiSong123 <xsong2008@126.com>
2026-05-26 12:01:24 +0000

    update LucidNFT

--- run_codex.sh
+++ run_codex.sh
@@ -1,6 +1,7 @@
 # Set environment variable
-export CODEX_API_KEY='sk-ant-oat01-d6f1024d2ea67a4b6b9ba44e93462e3c630dbc040db01b83d6e4954c37b07e55'
+# export CODEX_API_KEY='sk-ant-oat01-d6f1024d2ea67a4b6b9ba44e93462e3c630dbc040db01b83d6e4954c37b07e55'
 # export CODEX_API_KEY='sk-xzqptHv9PbeCYAWJ43HoklucBybCk6n7'
+export SSS_API_KEY="sk-sssaicode-8c09e70828aa03d69a5c086e1fa46bf5d2fa0ae1663f255fafa156a6f1eca795"
 
 # Run Codex
 codex
